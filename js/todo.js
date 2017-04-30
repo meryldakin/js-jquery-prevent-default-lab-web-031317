2af43be54e50a5ed234f0bfae6ebce318bb3b206
meryldakin <meryl.k.dakin@gmail.com>
2017-04-30 15:19:43 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
   // call functions here
+  listy()
 });
 
+
 // define functions here
+
+function listy(){
+  $('form#new-todo').on('submit', function(event){
+    event.preventDefault()
+    const $input = $('input#item').val()
+    const listItem = `<li>${$input}</li>`
+    $('ol').append(listItem)
+
+    // $input.val('')
+   })
+}
